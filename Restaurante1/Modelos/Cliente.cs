ac431978c8aeddffb05daa0d425fb75687bab7e6
m0iino <moino.cristian@gmail.com>
2021-02-28 21:30:49 -0600

    practica4

---
+++ Restaurante1/Modelos/Cliente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restaurante1.Modelos
+{
+    public class Cliente
+    {
+
+    }
+}
